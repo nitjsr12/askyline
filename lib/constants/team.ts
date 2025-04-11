179bc6dbb293953e110dbac037be1bb79b8710cf
Ravi <ravi@Ravis-MacBook-Air.local>
2025-04-11 19:47:45 +0530

    fixes

--- lib/constants/team.ts
+++ lib/constants/team.ts
@@ -1,10 +1,9 @@
 export const teamMembers = [
   {
-    name: "Aradhana Kushwaha",
+    name: "srabani",
     role: "CEO & Founder",
     bio: "With over 5 years of experience in digital transformation, Sarah leads our team with vision and innovation.",
-    image: "/images/profile.png"
-  },
+    image: "/images/
   {
     name: "Ravi Singh",
     role: "Technical Director",
